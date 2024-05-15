8eecf3a87f60ed968191b939db1db2cdc24db1fa
Samantha <144262012+samanthacxxz@users.noreply.github.com>
2024-05-15 13:06:22 +0200

    selecting signup elements

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,10 @@
 // IMPORTS
-
 import { validateLoginForm } from "./loginValidation.js";
-
-
 
 // SELECTING LOG IN FORM ELEMENTS
 
 const loginForm = document.querySelector('.form-login');
+const loginFormContainer = document.querySelector('.login-form-section');
 
 const emailInput = document.querySelector('.email');
 const passwordInput = document.querySelector('.password');
@@ -15,6 +13,20 @@ const loginButton = document.querySelector('.login-button');
 const emailError = document.querySelector('.email-error');
 const passwordError = document.querySelector('.password-error');
 const submissionError = document.querySelector('.submission-error');
+
+// SELECTING SIGN UP FORM ELEMENTS 
+
+const signupForm = document.querySelector('.signup-form');
+const signupFormContainer = document.querySelector('.signup-form-section');
+
+const signupFirstnameInput = document.querySelector('.signup-firstname');
+const signupLastnameInput = document.querySelector('.signup-lastname');
+const signupEmailInput = document.querySelector('.signup-email');
+const signupPasswordInput = document.querySelector('.signup-password');
+
+const signupError = document.querySelector('.signup-error');
+const closeSignupFormButton = document.querySelector('.signup-form-container__close');
+const openSignupFormButton = document.querySelector('.signup-form-container__open ');
 
 // EVENT LISTENER TO LOG IN BUTTON
 
