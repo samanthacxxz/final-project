bac5b8488405716efd2fea415d3d8a927167a7c0
Samantha <144262012+samanthacxxz@users.noreply.github.com>
2024-05-15 18:52:03 +0200

    adding firebase

---
+++ src/js/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAFad86HmK48XkBMDTkGwYHehUga_XDU60",
+    authDomain: "final-project-5f1e6.firebaseapp.com",
+    projectId: "final-project-5f1e6",
+    storageBucket: "final-project-5f1e6.appspot.com",
+    messagingSenderId: "705803816617",
+    appId: "1:705803816617:web:ac5e4bb3c132f49d73a3b8"
+};
+
+export default firebaseConfig
