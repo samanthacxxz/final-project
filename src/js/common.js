1ebc74f9e5e81d3700ae25a249df3fa1f8c49a85
Samantha <144262012+samanthacxxz@users.noreply.github.com>
2024-05-28 12:38:22 +0200

    event listener for account icon on header

--- src/js/common.js
+++ src/js/common.js
@@ -1,8 +1,9 @@
 import { handleHomePage } from "./index";
+import { handleLoginPage } from "./login";
 
 export function commonFunctionality(bodyClass){
     const homePageButton = document.querySelector('.header-title');
-    const myAccountButton = document.querySelector('.icon-log-container');
+    const myAccountButton = document.querySelector('.icon-account-container');
 
     // HANDLING BUTTOM REDIRECTING TO HOME PAGE
     homePageButton.addEventListener('click', (e) => {
@@ -10,7 +11,16 @@ export function commonFunctionality(bodyClass){
             handleHomePage();
         } else {
             // Redirect to the home page
-            window.location.href = 'http://127.0.0.1:3000/dist/index.html'; // Replace '/' with the actual URL of your home page
+            window.location.href = 'file:///Users/SamanthaCruz/sacr001/Sites/11/final-project/dist/index.html'; // Replace '/' with the actual URL of your home page
         }
     });
+
+    myAccountButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (bodyClass.contains('login-page')) {
+            handleLoginPage();
+        } else {
+            window.location.href = 'file:///Users/SamanthaCruz/sacr001/Sites/11/final-project/dist/login.html';
+        }
+    })
 };
