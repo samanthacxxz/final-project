a4e734a91d08eca4c06c566d187aa27d16289a26
Samantha <144262012+samanthacxxz@users.noreply.github.com>
2024-05-29 11:11:38 +0200

    adding event listener for signout button in header

--- src/js/common.js
+++ src/js/common.js
@@ -1,9 +1,13 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { handleHomePage } from "./index";
 import { handleLoginPage } from "./login";
 
 export function commonFunctionality(bodyClass){
+    const authService = getAuth();
+    
     const homePageButton = document.querySelector('.header-title');
     const myAccountButton = document.querySelector('.icon-account-container');
+    const signoutButtonIcon = document.querySelector('.button-log_out')
 
     // HANDLING BUTTOM REDIRECTING TO HOME PAGE
     homePageButton.addEventListener('click', (e) => {
@@ -18,9 +22,37 @@ export function commonFunctionality(bodyClass){
     myAccountButton.addEventListener('click', (e) => {
         e.preventDefault();
         if (bodyClass.contains('login-page')) {
-            handleLoginPage();
+            handleLoginPage(authService);
         } else {
             window.location.href = 'file:///Users/SamanthaCruz/sacr001/Sites/11/final-project/dist/login.html';
         }
     })
+
+    signoutButtonIcon.addEventListener('click', (e) => {
+        e.preventDefault();
+        signOut(authService)
+            .then(() => {
+                console.log('Signed out!');
+                signoutButtonIcon.style.display = 'none';
+                const loginButtonIcon = document.querySelector('.button-log_in');
+                loginButtonIcon.style.display = 'block';
+                window.location.href = 'login.html'; // Redirect to login page after signing out
+            })
+            .catch((err) => console.log(err.message));
+    });
+
+    onAuthStateChanged(authService, (user) => {
+        const signoutButtonIcon = document.querySelector('.button-log_out');
+        const loginButtonIcon = document.querySelector('.button-log_in');
+
+        if (user) {
+            // User is signed in
+            signoutButtonIcon.style.display = 'block';
+            loginButtonIcon.style.display = 'none';
+        } else {
+            // User is signed out
+            signoutButtonIcon.style.display = 'none';
+            loginButtonIcon.style.display = 'block';
+        }
+    });
 };
