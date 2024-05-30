4f6ddc406a098f5a82597e9f5d954b225ebf82ec
Samantha <144262012+samanthacxxz@users.noreply.github.com>
2024-05-30 21:28:16 +0200

    hide key final

--- src/js/firebaseConfig.js
+++ src/js/firebaseConfig.js
@@ -1,5 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAFad86HmK48XkBMDTkGwYHehUga_XDU60",
+    apiKey: `${AIzaSyAFad86HmK48XkBMDTkGwYHehUga_XDU60}`,
     authDomain: "final-project-5f1e6.firebaseapp.com",
     projectId: "final-project-5f1e6",
     storageBucket: "final-project-5f1e6.appspot.com",
