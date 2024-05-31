61c0a6094602b6dc7a625bcfc56205154ace9003
Samantha <144262012+samanthacxxz@users.noreply.github.com>
2024-05-31 05:02:49 +0200

    new run build

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/js/index.js',
+    entry: './dist/src/js/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
